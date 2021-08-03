e67844a764280a2d68fb0978376311e17f56615e
my name <me@example.com>
2021-08-03 23:39:53 +0530

    Add merge_sort.py

---
+++ Python/merge_sort.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left, right = arr[:mid], arr[mid:]
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+my_list = [54,26,93,17,77,31,44,55,20]
+merge_sort(my_list)
+print(my_list)
+
